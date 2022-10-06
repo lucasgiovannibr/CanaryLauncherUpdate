4ff2c44a7cc47e2a18a6cd82ad4fd7092cd46903
Beats <daniel15042014@hotmail.com>
2022-10-05 20:38:08 -0400

    fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Diagnostics;
-using System.Windows.Threading;
 using Newtonsoft.Json;
-using System.Web;
 
 namespace CanaryLauncherUpdate
 {
@@ -29,13 +18,13 @@ namespace CanaryLauncherUpdate
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		WebClient webClient = new WebClient();
+		readonly WebClient webClient = new();
 
 		bool clientDownloaded = false;
 		bool needUpdate = false;
-		string clientName = "client.exe";
-		string urlClient = "https://github.com/lucasgiovannibr/clientlauncherupdate/archive/refs/heads/main.zip";
-		string urlVersion = "https://raw.githubusercontent.com/lucasgiovannibr/clientlauncherupdate/main/version.txt";
+		readonly string clientName = "client.exe";
+		readonly string urlClient = "https://github.com/lucasgiovannibr/clientlauncherupdate/archive/refs/heads/main.zip";
+		readonly string urlVersion = "https://raw.githubusercontent.com/lucasgiovannibr/clientlauncherupdate/main/version.txt";
 		string currentVersion = "";
 
 		public MainWindow()
@@ -59,7 +48,7 @@ namespace CanaryLauncherUpdate
 
 			if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt"))
 			{
-				StreamReader reader = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt");
+				StreamReader reader = new(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt");
 				string? myVersion = reader.ReadLine();
 				reader.Close();
 
@@ -99,7 +88,7 @@ namespace CanaryLauncherUpdate
 			string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 			string json = path + "/CanaryClient/clientlauncherupdate-main/package.json";
 
-			using (StreamReader stream = new StreamReader(json))
+			using (StreamReader stream = new(json))
 			{
 				string jsonString = stream.ReadToEnd();
 				dynamic versionclient = JsonConvert.DeserializeObject(jsonString);
@@ -111,7 +100,7 @@ namespace CanaryLauncherUpdate
 			return "Play Game";
 		}
 
-		private void buttonPlay_Click(object sender, RoutedEventArgs e)
+		private void ButtonPlay_Click(object sender, RoutedEventArgs e)
 		{
 			if (needUpdate == true)
 			{
@@ -181,7 +170,7 @@ namespace CanaryLauncherUpdate
 		static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
 		static string SizeSuffix(Int64 value, int decimalPlaces = 1)
 		{
-			if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException("decimalPlaces"); }
+			if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException(nameof(decimalPlaces)); }
 			if (value < 0) { return "-" + SizeSuffix(-value, decimalPlaces); }
 			if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
 
@@ -198,11 +187,11 @@ namespace CanaryLauncherUpdate
 				SizeSuffixes[mag]);
 		}
 
-		private void buttonPlay_MouseEnter(object sender, MouseEventArgs e)
+		private void ButtonPlay_MouseEnter(object sender, MouseEventArgs e)
 		{
 			if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt"))
 			{
-				StreamReader reader = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt");
+				StreamReader reader = new(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt");
 				string? myVersion = reader.ReadLine();
 				reader.Close();
 
@@ -221,11 +210,11 @@ namespace CanaryLauncherUpdate
 			}
 		}
 
-		private void buttonPlay_MouseLeave(object sender, MouseEventArgs e)
+		private void ButtonPlay_MouseLeave(object sender, MouseEventArgs e)
 		{
 			if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt"))
 			{
-				StreamReader reader = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt");
+				StreamReader reader = new(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient/clientlauncherupdate-main/version.txt");
 				string? myVersion = reader.ReadLine();
 				reader.Close();
 
--- SplashScreen.xaml.cs
+++ SplashScreen.xaml.cs
@@ -1,21 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.IO;
-using System.IO.Compression;
 using System.Net;
-using System.Diagnostics;
-using System.Timers;
 using System.Windows.Threading;
 
 namespace CanaryLauncherUpdate
@@ -25,22 +11,21 @@ namespace CanaryLauncherUpdate
 	/// </summary>
 	public partial class SplashScreen : Window
 	{
-		WebClient webClient = new WebClient();
-
-		string urlClient = "https://github.com/lucasgiovannibr/clientlauncherupdate/archive/refs/heads/main.zip";
-		string urlVersion = "https://raw.githubusercontent.com/lucasgiovannibr/clientlauncherupdate/main/version.txt";
+		readonly WebClient webClient = new();
+		readonly string urlClient = "https://github.com/lucasgiovannibr/clientlauncherupdate/archive/refs/heads/main.zip";
+		readonly string urlVersion = "https://raw.githubusercontent.com/lucasgiovannibr/clientlauncherupdate/main/version.txt";
 		string currentVersion = "";
-		DispatcherTimer timer = new DispatcherTimer();
+		readonly DispatcherTimer timer = new();
 
 		public SplashScreen()
 		{
 			InitializeComponent();
-			timer.Tick += new EventHandler(timer_SplashScreen);
+			timer.Tick += new EventHandler(TimerSplashScreen);
 			timer.Interval = new TimeSpan(0, 0, 5); // 5 seconds
 			timer.Start();
 		}
 
-		public void timer_SplashScreen(object Source, EventArgs e)
+		public void TimerSplashScreen(object Source, EventArgs e)
 		{
 			// Check current version
 			currentVersion = webClient.DownloadString(urlVersion);
@@ -61,7 +46,7 @@ namespace CanaryLauncherUpdate
 			{
 				Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/CanaryClient");
 			}
-			MainWindow mainWindow = new MainWindow();
+			MainWindow mainWindow = new();
 			this.Close();
 			mainWindow.Show();
 			timer.Stop();
